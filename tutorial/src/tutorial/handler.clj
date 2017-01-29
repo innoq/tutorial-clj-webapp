a0f07c15e6977cb0323f183bcb08d788be10c09b
Joy Clark <joy.clark@innoq.com>
2017-01-29 16:09:17 +0100

    Updating tutorial to show title for chapter links and link to next step in the tutorial

--- tutorial/src/tutorial/handler.clj
+++ tutorial/src/tutorial/handler.clj
@@ -2,6 +2,7 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [clojure.java.io :as io]
+            [clojure.string :as str]
             [hiccup.page :refer [html5 include-css]]
             [hiccup.element :refer [link-to]]
             [markdown.core :refer [md-to-html-string]]
@@ -13,30 +14,43 @@
     (sort-by :name 
       (mapv (fn [f] {:name (.getName f) :text (slurp f)}) chapters))))
 
-(defn page [title content]
+(defn page [title & content]
   (html5 
     [:head 
      [:title title]
      (include-css "splendor.min.css")]      
     [:body content]))
 
+(defn first-line [text]
+  (re-find #"(?im)^.*$" text))
+
+(defn link-name [text]
+  (let [line (first-line text)]
+    (str/replace line #"^# " "")))
+
 (defn index []
   (page 
     "Clojure Tutorial"
-    [:div
-     [:h1 "Clojure Tutorial"]
-     [:ul
-       (for [chapter (load-chapters)]
-         [:li (link-to (:name chapter) (:name chapter))])]]))
+    [:h1 "Clojure Tutorial"]
+    [:ul
+      (for [chapter (load-chapters)]
+           [:li (link-to
+                  (:name chapter)
+                  (link-name (:text chapter)))])]))
 
 (defn show [chapter-name]
-  (let [chapter (first (filter #(= chapter-name (:name %)) (load-chapters)))]
+  (let [chapters (drop-while #(not= chapter-name (:name %)) (load-chapters))
+        chapter (first chapters)
+        next-chapter (second chapters)]
     (when chapter
       (page
         (:name chapter)
-        [:div
-         [:p (link-to "/" "Back to Tutorial")]
-         (md-to-html-string (:text chapter))]))))
+        [:p (link-to "/" "Back to Tutorial")
+            (md-to-html-string (:text chapter))]
+        (when next-chapter
+          [:p (link-to
+                (:name next-chapter)
+                (str "Next: " (link-name (:text next-chapter))))])))))
 
 (defroutes app-routes
   (GET "/" [] (index))
