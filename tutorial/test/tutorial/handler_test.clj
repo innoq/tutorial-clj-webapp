c4366b319bc31a674710574ff05994584624abab
Jan Stępień <jan.stepien@innoq.com>
2017-01-30 10:09:04 +0100

    Fix a failing test in the tutorial subdirectory

--- tutorial/test/tutorial/handler_test.clj
+++ tutorial/test/tutorial/handler_test.clj
@@ -1,5 +1,6 @@
 (ns tutorial.handler-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [ring.mock.request :as mock]
             [tutorial.handler :refer :all]))
 
@@ -7,7 +8,7 @@
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (str/includes? (:body response) "<h1>Clojure Tutorial</h1>"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
